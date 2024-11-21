3d0c3c9b74ceaf17e49c606fb74de04208249c32
tobias pflug <tobias.pflug@gmail.com>
2024-11-21 16:18:47 +0100

    provide flake.nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "My haskell application";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        haskellPackages = pkgs.haskellPackages;
+
+        jailbreakUnbreak = pkg:
+          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+
+        packageName = "spago-legacy";
+      in {
+        packages.${packageName} = # (ref:haskell-package-def)
+          pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.doJailbreak (haskellPackages.callCabal2nix packageName self rec {
+            # Dependency overrides go here
+          }));
+
+        defaultPackage = self.packages.${system}.${packageName};
+
+        devShell = pkgs.mkShell {
+          buildInputs = with haskellPackages; [
+            haskell-language-server
+            ghcid
+            cabal-install
+          ];
+          inputsFrom = builtins.attrValues self.packages.${system};
+        };
+      });
+}
